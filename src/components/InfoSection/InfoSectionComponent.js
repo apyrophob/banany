b568f3dbc02dd52a146d0b5ead779668ec8bae70
apyrophob <alekshkap@icloud.com>
2020-10-23 01:00:52 +0200

    rev fixes

--- src/components/InfoSection/InfoSectionComponent.js
+++ src/components/InfoSection/InfoSectionComponent.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import { ImageComponent } from '../Image/Image'
-import { LinkComponent } from '../Link/Link'
+// import { LinkComponent } from '../Link/Link'
 
 export const InfoSectionComponent = props => {
     const { linkHref, imgSrc } = props;
-
-    const addComponentByProps = () => (props.linkHref) ? (<LinkComponent href={linkHref} />) :
-        (props.imgSrc) ? (<ImageComponent src={imgSrc} />) :
-            (<LinkComponent href={linkHref} />, <ImageComponent src={imgSrc} />)
-        ;
 
     return (
         <div className='info-section'>
             <div className='info-section-text'>Block of Text.</div>
             <div className='info-section-component'>
-                {addComponentByProps}
+                {linkHref &&
+                    (<LinkComponent href={linkHref} />)
+                }
+                {imgSrc &&
+                    (<ImageComponent src={imgSrc} />)
+                }
             </div>
         </div>
     );
